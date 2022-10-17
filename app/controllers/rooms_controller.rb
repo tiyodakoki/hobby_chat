7af91e966c6311e2ca0e9845397e8f8ca55fd249
tiyodakoki <kagunatsu@gmail.com>
2022-10-17 20:38:23 +0900

    修正

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,19 @@
 class RoomsController < ApplicationController
+  def index
+    @rooms = Room.all
+  end
+  def new
+    @room = Room.new
+  end
+  def create
+    @room = Room.new(room_params)
+    @room.save
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:room_name, :concept, :category_id, :area_id)
+  end
+
 end
+
